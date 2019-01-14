3cea535315a55a6985c1cab223aa2945dd059f74
Nick Podlesny <podlesnyy.nikita@gmail.com>
2019-01-14 15:49:16 +0300

    save Matrix

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -2,18 +2,86 @@
 #define _Matrix_h_
 
 template <class ValType>
-class TMatrix : public TVector<TVector<ValType> >
+class TMatrix : public TVector<TVector<ValType>>
 {
 public:
-	TMatrix(int s = 10);
-	TMatrix(const TMatrix &mt); // копирование
-	TMatrix(const TVector<TVector<ValType> > &mt); // преобразование типа
-	TMatrix& operator==(const TMatrix &mt); // сравнение
-	TMatrix& operator= (const TMatrix &mt); // присваивание
-	TMatrix operator+ (const TMatrix &mt); // сложение
-	TMatrix operator- (const TMatrix &mt); // вычитание
-	TMatrix operator* (const TMatrix &mt); // умножение
-};
+	TMatrix(int s = 10) {
+		this->size = s;
+		this->startIndex = 0;
+		this->pVector = new TVector<ValType>[s];
+		for (int i = 0; i < this->size; i++) {
+			TVector<ValType> t(s, i);
+			this->pVector[i] = t;
+		}
+
+	}
+
+	TMatrix(const TMatrix &mt) { // копирование
+
+		this->size = mt.size;
+		this->startIndex = mt.startIndex;
+		this->pVector = new TVector<ValType>[mt.size];
+		for (int i = 0; i < this->size; i++)
+			this->pVector[i] = mt.pVector[i];
+	}
+
+	TMatrix(const TVector<TVector<ValType> > &mt) { // преобразование типа
+		return mt.pVector;
+	} 
+
+	TMatrix& operator==(const TMatrix &mt){ // сравнение
+		for (int i = 0; i < this->size; i++)
+			if (this->pVector[i] != mt.pVector[i])
+				return 0;
+		return 1;
+	} 
+
+	TMatrix& operator= (const TMatrix &mt) { // присваивание
+		if (this != &mt)
+		{
+			if (this->size != mt.size)
+			{
+				delete[] this->pVector;
+				this->pVector = new TVector<ValType>[mt.size];
+			}
+			this->size = mt.size;
+			this->startIndex = mt.startIndex;
+			for (int i = 0; i < this->size; i++)
+				this->pVector[i] = mt.pVector[i];
+		}
+		return *this;
+	} 
+
+	TMatrix operator+ (const TMatrix &mt) { // сложение
+		TMatrix temp(this->size);
+		for (int i = 0; i < this->size; i++)
+			temp.pVector[i] = this->pVector[i] + mt.pVector[i];
+		return temp;
+	}
+
+	TMatrix operator- (const TMatrix &mt) { // вычитание
+		TMatrix temp(this->size);
+		for (int i = 0; i < this->size; i++)
+			temp.pVector[i] = this->pVector[i] - mt.pVector[i];
+		return temp;
+	}
+
+	TMatrix operator* (const TMatrix &mt) { // умножение
+		TMatrix temp(this->size);
+		for (int i = 0; i < this->size; i++)
+			temp.pVector[i] = this->pVector[i] * mt.pVector[i];
+		return temp;
+	}
+
+	friend std::ostream & operator<<(std::ostream &out, const TMatrix &mt) {
+		TMatrix mt1(mt);
+		for (int i = 0; i < mt1.getSize(); i++) {
+			out << mt1[i] << std::endl;
+		}
+		return out;
+	}
+};
+
 
 #endif
 
--- Matrix/Vector.h
+++ Matrix/Vector.h
@@ -1,6 +1,8 @@
 #ifndef _Vector_h_
 #define _Vector_h_
 
+#include <iostream>
+
 template <class ValType>
 class TVector
 {
@@ -9,24 +11,119 @@ protected:
 	int size; // размер вектора
 	int startIndex; // индекс первого элемента вектора
 public:
-	TVector(int s = 10, int si = 0);
-	TVector(const TVector &v); // конструктор копирования
-	~TVector();
+	TVector(int s = 10, int si = 0) {
+		size = s;
+		startIndex = si;
+		this->pVector = new ValType[s];
+	}
+
+	TVector(const TVector &v) { // конструктор копирования
+		this->size = v.size;
+		this->startIndex = v.startIndex;
+		this->pVector = new ValType[v.size];
+		for (int i = 0; i < this->size; i++)
+			pVector[i] = v.pVector[i];
+
+	}
+
+	~TVector() {
+		delete[] pVector;
+	}
+
 	int getSize() { return size; } // размер вектора
+
 	int getStartIndex() { return startIndex; } // индекс первого элемента
-	ValType& getValue(int pos); // доступ с контролем индекса
-	ValType& operator[](int pos); // доступ
-	int operator==(const TVector &v); // сравнение
-	TVector& operator= (const TVector &v); // присванивание
+
+	ValType& getValue(int pos) { // доступ с контролем индекса
+		return pVector[pos];
+		startIndex++;
+	}
+
+	ValType& operator[](int pos) { // доступ
+		return pVector[pos];
+	} 
+
+	int operator==(const TVector &v) { // сравнение
+		int max_size = (this->size > v.getSize())? this->size  : v.getSize();
+		for (int i = 0; i < max_size; i++)
+			if (this->pVector[i] != v.pVector[i])
+				return 0;
+		return 1;
+	}
+
+	TVector& operator= (const TVector &v) { // присванивание
+		if (this != &v)
+		{
+			if (size != v.size)
+			{
+				delete[] pVector;
+				pVector = new ValType[v.size];
+			}
+			size = v.size;
+			startIndex = v.startIndex;
+			for (int i = 0; i < size; i++)
+				pVector[i] = v.pVector[i];
+		}
+		return *this;
+	}
+
 	// скалярные операции
-	TVector operator+ (const ValType &val); // прибавить скаляр
-	TVector operator- (const ValType &val); // вычесть скаляр
-	TVector operator* (const ValType &val); // умножить на скаляр
+	TVector operator+ (const ValType &val) {  // прибавить скаляр
+		TVector temp(size, startIndex);
+		for (int i = 0; i < size; i++)
+			temp.pVector[i] = pVector[i] + val;
+		return temp;
+	} 
+
+	TVector operator- (const ValType &val) { // вычесть скаляр
+		TVector temp(size, startIndex);
+		for (int i = 0; i < size; i++)
+			temp.pVector[i] = pVector[i] - val;
+		return temp;
+	} 
+
+	TVector operator* (const ValType &val) { // умножить на скаляр
+		TVector temp(size, startIndex);
+		for (int i = 0; i < size; i++)
+			temp.pVector[i] = pVector[i] * val;
+		return temp;
+	} 
+
 	// векторные операции
-	TVector operator+ (const TVector &v); // сложение
-	TVector operator- (const TVector &v); // вычитание
-	TVector operator* (const TVector &v); // скалярное произведение
+	TVector operator+ (const TVector &v) {  // сложение
+		TVector temp(size, startIndex);
+		for (int i = 0; i < size; i++)
+			temp.pVector[i] = pVector[i] + v.pVector[i];
+		return temp;
+	} 
+
+	TVector operator- (const TVector &v) {  // вычитание
+		TVector temp(size, startIndex);
+		for (int i = 0; i < size; i++)
+			temp.pVector[i] = pVector[i] - v.pVector[i];
+		return temp;
+	}
+	
+	TVector operator* (const TVector &v) {  // скалярное произведение
+		TVector temp(1, startIndex);
+		int sum = 0;
+		for (int i = 0; i < size; i++)
+			sum += pVector[i] * v.pVector[i];
+		temp[0] = sqrt((double)sum);
+		return temp;
+	}
+	
 	// ввод-вывод
+	friend std::ostream& operator<<(std::ostream &out, const TVector &v) {
+		TVector v1(v);
+		for (int i = v1.getStartIndex(); i < v1.getSize(); i++) {
+			out << v1[i] << "\t";
+		}
+		return out;
+	}
+
+
 };
+
 
 #endif
